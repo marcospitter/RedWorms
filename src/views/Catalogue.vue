c5ba2747fed45af97448cd4002cf25c340c189b9
Marcos Pitter <mpitter96@gmail.com>
2021-09-29 20:14:23 -0300

    refactor(ui): refactor seeds and Product components

--- src/views/Catalogue.vue
+++ src/views/Catalogue.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <Products />
+    <Products/>
     <Seeds/>
   </div>
 </template>
 
 <script>
 
-import Products from '../components/catalogue/Products.vue'
-import Seeds from '../components/catalogue/Seeds.vue'
+import Products from '../components/catalogue/products/Products.vue'
+import Seeds from '../components/catalogue/seeds/Seeds.vue'
 
 export default {
   components: { Products, Seeds },

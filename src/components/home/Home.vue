f0f10761b05071b41e693bc7638640be90d75af0
Marcos Pitter <mpitter96@gmail.com>
2021-09-29 20:23:40 -0300

    refactor(ui): add file .css .js home component

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -69,42 +69,5 @@
   </div>
 </template>
 
-<style>
-  span {
-    font-weight: bold;
-  }
-  p {
-    text-align: justify;
-    margin: 2em auto;
-  }
-  h3 {
-    margin: 2em auto;
-  }
-</style> 
-<script>
-
-export default {
-  data () {
-      return {
-        items: [
-          {
-            src: 'https://cdn.pixabay.com/photo/2018/09/08/22/41/compost-3663514_960_720.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2019/10/31/06/41/compost-4591094_960_720.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2016/01/14/20/57/worm-1140767_960_720.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2021/02/26/21/09/compost-6053136_960_720.jpg',
-          },
-          {
-            src: 'https://cdn.pixabay.com/photo/2016/04/02/13/22/seeds-1302793_960_720.jpg',
-          },
-          
-        ],
-      }
-    },
-}
-</script>
+<style src="./home.css"></style> 
+<script src="./home.js"></script>
---
+++ src/components/home/home.js
@@ -0,0 +1,24 @@
+export default {
+    data () {
+        return {
+          items: [
+            {
+              src: 'https://cdn.pixabay.com/photo/2018/09/08/22/41/compost-3663514_960_720.jpg',
+            },
+            {
+              src: 'https://cdn.pixabay.com/photo/2019/10/31/06/41/compost-4591094_960_720.jpg',
+            },
+            {
+              src: 'https://cdn.pixabay.com/photo/2016/01/14/20/57/worm-1140767_960_720.jpg',
+            },
+            {
+              src: 'https://cdn.pixabay.com/photo/2021/02/26/21/09/compost-6053136_960_720.jpg',
+            },
+            {
+              src: 'https://cdn.pixabay.com/photo/2016/04/02/13/22/seeds-1302793_960_720.jpg',
+            },
+            
+          ],
+        }
+    },
+}
